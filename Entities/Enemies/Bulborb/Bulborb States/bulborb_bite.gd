3f308fca697d5bececd16a4a6b0fc51085b4549f
Perturbo42 <calvin.kim6@gmail.com>
2026-07-22 17:10:48 +0900

    bulborb attacking
    pikmin and olimar taking damage next

--- Entities/Enemies/Bulborb/Bulborb States/bulborb_bite.gd
+++ Entities/Enemies/Bulborb/Bulborb States/bulborb_bite.gd
@@ -1,13 +1,52 @@
 class_name BulborbBite extends BulborbState
+@onready var timer: Timer = $Timer
+@onready var bite_area: Area2D = $"../../BiteArea"
+@onready var detection_area: Area2D = $"../../DetectionArea"
+var attacking: bool = false
 
 func enter():
+	attacking = false
+	timer.timeout.connect(attack)
+	timer.start(0.2)
 	pass
 
 func update(_delta: float):
 	pass
 
 func physics_update(_delta: float):
+	if !bulborb.chase_target or attacking == true:
+		return
+	var bulb_pos: Vector2 = bulborb.global_position
+	var targ_pos: Vector2 = bulborb.chase_target.global_position
+	
+	var dir: Vector2 = bulb_pos.direction_to(targ_pos)
+	
+	if bulb_pos.distance_to(targ_pos) >= 20:
+		bulborb.velocity = bulborb.speed * dir
+	bulborb.move_and_slide()
 	pass
 
 func exit():
+	attacking = false
+	pass
+
+func attack():
+	attacking = true
+	
+	for body in bite_area.get_overlapping_bodies():
+		if body == bulborb.chase_target:
+			if body is Pikmin:
+				body.take_damage()
+			elif body is Olimar:
+				body.take_damage()
+	
+	if detection_area.get_overlapping_bodies():
+		var new_body = detection_area.get_overlapping_bodies()[0]
+		bulborb.chase_target = new_body
+	else:
+		finished.emit(RETURN)
+		return
+	
+	attacking = false
+	timer.start(3.0)
 	pass
--- Entities/Enemies/Bulborb/Bulborb States/bulborb_chase.gd
+++ Entities/Enemies/Bulborb/Bulborb States/bulborb_chase.gd
@@ -8,10 +8,16 @@ func update(_delta: float):
 	pass
 
 func physics_update(_delta: float):
-	if bulborb.chase_target != null:
-		var dir = bulborb.global_position.direction_to(bulborb.chase_target.global_position)
-		bulborb.velocity = bulborb.speed * dir
+	if !bulborb.chase_target:
+		return
+	var bulb_pos: Vector2 = bulborb.global_position
+	var targ_pos: Vector2 = bulborb.chase_target.global_position
 	
+	var dir: Vector2 = bulb_pos.direction_to(targ_pos)
+	bulborb.velocity = bulborb.speed * dir
+	
+	if bulb_pos.distance_to(targ_pos) <= 20:
+		finished.emit(BITE)
 	bulborb.move_and_slide()
 	pass
 
