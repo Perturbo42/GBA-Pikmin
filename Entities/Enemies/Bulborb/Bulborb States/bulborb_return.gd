59a4b3aff5a12a25c9f27481f756c2aff6b76f7a
Perturbo42 <calvin.kim6@gmail.com>
2026-08-20 15:57:19 +0900

    return home component

--- Entities/Enemies/Bulborb/Bulborb States/bulborb_return.gd
+++ Entities/Enemies/Bulborb/Bulborb States/bulborb_return.gd
@@ -1,5 +1,5 @@
 class_name BulborbReturn extends BulborbState
-@export var close_to_home: int = 40
+@export var return_home_component: Node
 
 func enter():
 	pass
@@ -8,14 +8,7 @@ func update(_delta: float):
 	pass
 
 func physics_update(_delta: float):
-	var dist_to_home: float = bulborb.global_position.distance_to(bulborb.home_location)
-	if dist_to_home <= close_to_home:
-		finished.emit(IDLE)
-	else:
-		var dir = bulborb.global_position.direction_to(bulborb.home_location)
-		bulborb.velocity = dir * bulborb.speed
-	
-	bulborb.move_and_slide()
+	return_home_component.return_home()
 	pass
 
 func exit():
---
+++ Entities/Enemies/Enemy Component/Return Home Component/return_home_component.gd
@@ -0,0 +1,18 @@
+class_name ReturnHomeComponent extends Node
+@export_group("External Properties")
+@export var enemy: Enemy
+@export var idle_state: State
+
+@export_group("Internal Properties")
+@export var close_to_home: float
+
+func return_home():
+	var dist_to_home: float = enemy.global_position.distance_to(enemy.home_location)
+	if dist_to_home <= close_to_home:
+		enemy.finished.emit(idle_state.name)
+	else:
+		var dir = enemy.global_position.direction_to(enemy.home_location)
+		enemy.velocity = dir * enemy.speed
+	
+	enemy.move_and_slide()
+	pass
